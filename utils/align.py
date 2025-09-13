e60cb3067c1c22aa49e17a3a4ad42daed59ea845
harish <rawatu74@gmail.com>
2025-09-13 22:58:11 +0530

    train cyclegan

--- utils/align.py
+++ utils/align.py
@@ -10,10 +10,10 @@ class AlignedEmbroideryDataset(Dataset):
         self.root_dir = root_dir
         self.mask_dir = mask_dir
         self.image_size = image_size
-        
+
         # Get all image files
         self.image_files = [
-            os.path.join(root_dir, f) for f in os.listdir(root_dir) 
+            os.path.join(root_dir, f) for f in os.listdir(root_dir)
             if f.lower().endswith((".png", ".jpg", ".jpeg"))
         ]
         self.image_files.sort()
@@ -23,12 +23,7 @@ class AlignedEmbroideryDataset(Dataset):
             transforms.Resize((image_size, image_size)),
             transforms.ToTensor(),  # converts to [0,1]
         ])
-        
-        # Transform for masks (grayscale)
-        self.mask_transform = transforms.Compose([
-            transforms.Resize((image_size, image_size)),
-            transforms.ToTensor(),
-        ])
+
 
     def __len__(self):
         return len(self.image_files)
@@ -47,21 +42,4 @@ class AlignedEmbroideryDataset(Dataset):
         target_tensor = self.transform(target_img)
         input_tensor = self.transform(input_img)
 
-        # Load precomputed mask
-        mask = self._load_mask(img_path)
-
-        return input_tensor, target_tensor, mask
-    
-    def _load_mask(self, img_path):
-        """Load precomputed mask from masks folder"""
-        mask_filename = os.path.basename(img_path)
-        mask_path = os.path.join(self.mask_dir, mask_filename)
-        
-        # Load mask image and convert to tensor
-        mask_img = Image.open(mask_path).convert('L')  # Grayscale
-        mask_tensor = self.mask_transform(mask_img)
-        
-        # Ensure mask is binary (0 or 1)
-        mask_tensor = (mask_tensor > 0.5).float()
-        
-        return mask_tensor
+        return {'A': input_tensor,'B': target_tensor,'A_paths': img_path,'B_paths': img_path}
